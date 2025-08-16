cd0ae55a64280ef4479d8ca22491b6f5a57945a0
Mustapha Boutoub || killux <mustaphaboutoubdev@gmail.com>
2025-08-16 15:43:20 +0100

    pentagon

--- Main.java
+++ Main.java
@@ -22,16 +22,21 @@ public class Main {
         // point1.draw(image);
         Line line = new Line(point1, point2);
         line.draw(image);
-        // Rectangle rectangle = new Rectangle(new Point(50, 50), new Point(300, 200));
-        // rectangle.draw(image);
-        // Triangle triangle = new Triangle(new Point(100, 100), new Point(900, 900),
-        //         new Point(100, 900));
-        // triangle.draw(image);
+        Rectangle rectangle = new Rectangle(new Point(50, 50), new Point(300, 200));
+        rectangle.draw(image);
+        Triangle triangle = new Triangle(new Point(100, 100), new Point(900, 900),
+                new Point(100, 900));
+        triangle.draw(image);
 
-        // for (int i = 0; i < 50; i++) {
-        //     Circle circle = Circle.random(image.getWidth(), image.getHeight());
-        //     circle.draw(image);
-        // }
+        for (int i = 0; i < 50; i++) {
+            Circle circle = Circle.random(image.getWidth(), image.getHeight());
+            circle.draw(image);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            Pentagon pen = Pentagon.random(image.getWidth(), image.getHeight());
+            pen.draw(image);
+        }
         image.save("image.png");
     }
 }
---
+++ Pentagon.java
@@ -0,0 +1,41 @@
+
+import java.awt.Color;
+
+public class Pentagon implements Drawable {
+    Point center;
+    int radius;
+
+    public Pentagon(Point point, int radius) {
+        this.center = point;
+        this.radius = radius;
+    }
+
+    public static Pentagon random(int width, int height) {
+        return new Pentagon(Point.random(width, height), (int) (Math.random() * Math.max(width, height)));
+    }
+
+    @Override
+    public void draw(Displayable image) {
+        int last_x = radius + center.x;
+        int last_y = center.y;
+        Color color = getColor();
+
+        for (int i = 0; i <= 5; i++) {
+            Point last_point = new Point(last_x,last_y);
+            double angle =  ((double) i * (360. / 5.) * Math.PI ) / 180. ;
+            int x = (int) Math.floor(radius * Math.cos(angle) + (double) center.x) ;
+            int y = (int) Math.floor(radius * Math.sin(angle) + (double) center.y) ;
+            last_x = x;
+            last_y = y;
+            Point next_point = new Point(x,y);
+            Line line = new Line(last_point, next_point);
+            line.color = color;
+            line.draw(image);
+        }
+    }
+
+    @Override
+    public Color getColor() {
+        return new Color((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255));
+    }
+}
